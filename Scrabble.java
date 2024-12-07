f04937bd9a4325a02b6421fc399af803e259503a
guy michaeli <guy.michaeli@post.runi.ac.il>
2024-12-07 15:28:04 +0200

    Update Scrabble.java

--- Scrabble.java
+++ Scrabble.java
@@ -56,7 +56,7 @@ public class Scrabble {
 		
 		for (int i=0; i<NUM_OF_WORDS; i ++)
 		{
-			if (DICTIONARY[i].equals(word.toLowerCase()))
+			if (DICTIONARY[i].equals(word))
 			{
 				
 				return true;
